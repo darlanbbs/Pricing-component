98b9aab5cb469f138bbfc85e02f364bf828c9052
darlanbbs <109982294+darlanbbs@users.noreply.github.com>
2022-12-29 10:21:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,16 @@
 let toggle = document.querySelector('.toggle')
 let label = document.querySelector('.label')
-let dolar = document.querySelector('.dolar')
-let anual1 = document.querySelector('.annualy1')
-let anual2 = document.querySelector('.annualy2')
-let anual3 = document.querySelector('.annualy3')
-let preco1 = document.querySelector('.preco1')
-let preco2 = document.querySelector('.preco2')
-let preco3 = document.querySelector('.preco3')
+let precos = document.querySelectorAll('.preco')
+let anual = document.querySelectorAll('.annualy')
 
 document.querySelector(".label").onclick = function () {
-    if (toggle.checked) {
-        // mensal
-        anual1.style.display = 'none'
-        anual2.style.display = 'none'
-        anual3.style.display = 'none'
-        preco1.style.display = 'flex'
-        preco2.style.display = 'flex'
-        preco3.style.display = 'flex'
-    }else{
-    //    anual
-    anual1.style.display = 'flex'
-    anual2.style.display = 'flex'
-    anual3.style.display = 'flex'
-    preco1.style.display = 'none'
-    preco2.style.display = 'none'
-    preco3.style.display = 'none'
+        if (!toggle.checked) {
+            precos.forEach(element => element.classList.toggle('hide'))
+            anual.forEach(element => element.classList.remove('hide'))
+        }
+        else{
+            precos.forEach(element => element.classList.remove('hide'))
+            anual.forEach(element => element.classList.toggle('hide'))
+        }
     }
-}
+
